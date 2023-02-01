6a3cf599336c79d9e0e3243de8e01f46385d6fc5
iqbal9865 <iqbalahmed9865@gmail.com>
2023-02-01 15:48:12 +0600

    consistant dropdown with code refactor

--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 
-
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -9,4 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class NavbarComponent implements OnInit {
   constructor() {}
   ngOnInit(): void {}
+  openDropDown: boolean = false;
+  profileDropDownToggle() {
+    this.openDropDown = !this.openDropDown;
+  }
 }
