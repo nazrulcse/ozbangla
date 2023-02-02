0a9f0de34be8041abdc22619f6adb053a7ca95ce
iqbal9865 <iqbalahmed9865@gmail.com>
2023-02-02 19:08:37 +0600

    chat component added

--- src/app/components/dashboard/chat/chat.component.ts
+++ src/app/components/dashboard/chat/chat.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { faWindowClose, faCog, faTimes, faUser, faSearch } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-chat',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chat.component.scss']
 })
 export class ChatComponent implements OnInit {
-
+  @Output() closeChat = new EventEmitter<boolean>();
+  faCog = faCog;
+  faWindowClose = faWindowClose;
+  faTimes = faTimes;
+  faUser = faUser;
+  faSearch = faSearch;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  closeChatCanvas() {
+    this.closeChat.emit(false);
+  }
 }
--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -11,19 +11,34 @@ export class NavbarComponent implements OnInit {
   openProfleDropDown: boolean = false;
   settingDropDown: boolean = false;
   showNotification: boolean = false;
+  showMessages: boolean = false;
   profileDropDownToggle() {
     this.openProfleDropDown = !this.openProfleDropDown;
     this.settingDropDown = false;
     this.showNotification = false;
+    this.showMessages = false;
   }
   settingsDropDownToggle() {
     this.settingDropDown = !this.settingDropDown;
     this.openProfleDropDown = false;
     this.showNotification = false;
+    this.showMessages = false;
   }
   showNotifications() {
     this.showNotification = !this.showNotification;
     this.settingDropDown = false;
     this.openProfleDropDown = false;
+    this.showMessages = false;
+  }
+  showAllMessages() {
+    this.showMessages = true;
+    this.settingDropDown = false;
+    this.openProfleDropDown = false;
+    this.showNotification = false;
+    console.log(this.showMessages)
+  }
+
+  chatClose(event: any) {
+    this.showMessages = event;
   }
 }
